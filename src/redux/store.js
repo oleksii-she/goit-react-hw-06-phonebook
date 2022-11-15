996399c2ecef71ff4b114c37bb4e4c9fcf9315e5
Oleksii Shevchenko <vazerius@gmail.com>
2022-11-15 21:54:59 +0200

    +

---
+++ src/redux/store.js
@@ -0,0 +1,7 @@
+//1. викликаємо імпорт, налаштування стора configureStore
+import { configureStore } from '@reduxjs/toolkit';
+
+//1.  створюємо налаштування для стора
+export const store = configureStore({
+  reducer: {},
+});
